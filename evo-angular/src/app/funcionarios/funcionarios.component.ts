6cdeb1bce6d02e275eb9c362d5076f20c01b223e
vituhonda <vitorutk@gmail.com>
2024-01-29 14:45:49 -0300

    build(project): created funcionarios component #8

---
+++ evo-angular/src/app/funcionarios/funcionarios.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-funcionarios',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './funcionarios.component.html',
+  styleUrl: './funcionarios.component.css'
+})
+export class FuncionariosComponent {
+
+}
