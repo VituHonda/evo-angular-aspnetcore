9d8cc1e3de551f8f65771778f056ee4470a82898
vituhonda <vitorutk@gmail.com>
2024-01-29 15:21:58 -0300

    build(project): Permited CORS #19

--- evo-aspnetcore/evo-aspnetcore/Controllers/FuncionarioController.cs
+++ evo-aspnetcore/evo-aspnetcore/Controllers/FuncionarioController.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-
 [Route("[controller]")]
 [ApiController]
 public class FuncionarioController : ControllerBase
