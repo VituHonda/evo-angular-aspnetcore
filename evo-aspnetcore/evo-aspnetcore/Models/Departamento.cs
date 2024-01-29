a911a9461db06fe22efae94ae7d9216d4f406a5e
vituhonda <vitorutk@gmail.com>
2024-01-29 14:56:05 -0300

    build(project): created models for departamento and funcionario #11 #12

---
+++ evo-aspnetcore/evo-aspnetcore/Models/Departamento.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EvoTest.Models
+{
+    public class Departamento
+    {
+        [Key]
+        public int? Id { get; set; }
+        public string? Nome { get; set; }
+        public string? Sigla { get; set; }
+        public List<Funcionario>? Funcionarios { get; set; }
+
+        public Departamento() { }
+        public Departamento(int id, string nome, string sigla)
+        {
+            this.Id = id;
+            this.Nome = nome;
+            this.Sigla = sigla;
+        }
+    }
+}
